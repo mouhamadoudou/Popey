bfc7ede6ee94e95a06e7f70fc93efb29ce2950c8
Mouhamadou Niang <91889737+mouhamadoudou@users.noreply.github.com>
2022-11-13 17:13:30 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";*/
+import { NavBar } from "./components/NavBar";
+
+const App = () => {
+  return (
+    <div className="App">
+        <NavBar />
+    </div>
+  );
+};
+
+export default App;
